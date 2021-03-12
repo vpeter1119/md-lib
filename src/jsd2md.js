0f11411eaf6afa0b6f7c00e9097428e34a877884
vpeter1119 <vpeter1119@gmail.com>
2021-03-12 10:52:00 +0100

    feat: Set up automatic documentation

--- src/jsd2md.js
+++ src/jsd2md.js
@@ -1,6 +1,7 @@
 const jsdoc2md = require('jsdoc-to-markdown');
 const fs = require('fs');
 
+const MDLib = require('./modules/compiler');
 const version = require('../package.json').version;
 
 const options = {
@@ -8,7 +9,17 @@ const options = {
 }
 
 jsdoc2md.render(options).then(result => {
-    fs.writeFileSync(`./docs/${version}/README.md`, result);
+    let dir = `./docs/${version}`;
+    fs.exists(dir, exists => {
+        if (!exists) {
+            fs.mkdirSync(dir);
+            fs.writeFileSync(`${dir}/README.md`, result);
+            MDLib.compile();
+        } else {
+            fs.writeFileSync(`${dir}/README.md`, result);
+            MDLib.compile();
+        }
+    })
 }, error => {
     console.log(error);
 });
