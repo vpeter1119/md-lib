d883cd2bbfad4094bc4c34dfe0ac96ad742f74ca
Peter Vertesi <vpeter1119@gmail.com>
2021-03-16 19:02:17 +0100

    fix: Create separate folder for tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,12 @@
 const MDLib = require('./index');
 const correctVersion = require('../package.json').version;
 
-test('returns a version', () => {
+test('returns the correct version', () => {
     const returnedVersion = MDLib.version;
     expect(returnedVersion).toBe(correctVersion);
+})
+
+test('has the Compile function', () => {
+    let fn = MDLib.Compile;
+    expect(typeof(fn)).toBe('function');
 })
